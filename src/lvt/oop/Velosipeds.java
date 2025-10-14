b357ec9132ea4c7fda3835d5c0092721fd4cff9c
Gihaluf <gihaluf.bojar@gmail.com>
2025-10-14 14:32:50 +0300

    Iesākta velosipēda klase

---
+++ src/lvt/oop/Velosipeds.java
@@ -0,0 +1,51 @@
+package lvt.oop;
+
+import java.util.Random;
+
+public class Velosipeds {
+	//atribūti
+	private int ritenaD, sedeklaPoz;
+	private double atrumsMS, cena;
+	private String razotajs;
+	
+	//kostruktors
+	public Velosipeds(int ritenaD, int sedeklaPoz,
+				double cena, String razotajs) {
+		this.ritenaD = ritenaD;
+		this.sedeklaPoz = sedeklaPoz;
+		atrumsMS = kustiba();
+		this.cena = cena;
+		this.razotajs = razotajs;
+	}
+
+	//metodes
+	public int noteiktRitenaD() {
+		return ritenaD;
+	}
+	public int noteiktSedeklaPoz() {
+		return sedeklaPoz;
+	}
+	public String noteiktRaz() {
+		return razotajs;
+	}
+	public double noteiktCenu() {
+		return cena;
+	}
+	public double kustiba() {
+		Random rand = new Random();
+		double celsM, laiksS;
+		celsM = rand.nextDouble()*(1000-1)+1;
+		laiksS = rand.nextDouble()*(60-10)+10;
+		return Math.round(celsM/laiksS);
+	}
+	public double noteiktAtr() {
+		return atrumsMS;
+	}
+	public String izvadit() {
+		return "Riteņa ražotājs: "+noteiktRaz()+
+				"\nRiteņa cena: "+noteiktCenu()+
+				"\nRiteņa leilums collās: "+noteiktRitenaD()+
+				"\nIEstatītais sēdekļa garums: "+noteiktSedeklaPoz()+
+				"\nKustības ātrums: "+noteiktAtr()+"m/s";
+	}
+}
