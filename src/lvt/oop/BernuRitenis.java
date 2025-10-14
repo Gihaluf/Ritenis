22f5a3133854547f80bf5071a09c35eb226598a5
Gihaluf <gihaluf.bojar@gmail.com>
2025-10-14 15:56:59 +0300

    Pievienota pārbaude

--- src/lvt/oop/BernuRitenis.java
+++ src/lvt/oop/BernuRitenis.java
@@ -1,5 +1,14 @@
 package lvt.oop;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JOptionPane;
 
 public class BernuRitenis extends Velosipeds{
@@ -25,6 +34,21 @@ public class BernuRitenis extends Velosipeds{
 			JOptionPane.showMessageDialog(null, 
 					"Darbība nav iespējama", "Paziņojums",
 					JOptionPane.ERROR_MESSAGE);
+		}
+	}
+	
+	public void zvanitZvaninu() throws MalformedURLException, 
+	UnsupportedAudioFileException, IOException, 
+	LineUnavailableException{
+		if(zvanins) {
+			File f = new File(".//audio"+"ring.wav");
+			AudioInputStream ais = AudioSystem.getAudioInputStream(f.toURI().toURL());
+			Clip c = AudioSystem.getClip();
+			c.open(ais);
+			c.start();
+		}else {
+			JOptionPane.showMessageDialog(null, "Nav uzstādīts zvaniņš!", 
+					"Paziņojums", JOptionPane.ERROR_MESSAGE);
 		}
 	}
 }
